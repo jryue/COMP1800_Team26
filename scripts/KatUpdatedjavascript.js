5aa36bc307a86b089a6694ab125323a20759ca5e
Kat Sukkau <Katsukkau@gmail.com>
2020-11-07 21:54:06 -0800

    added map

--- scripts/KatUpdatedjavascript.js
+++ scripts/KatUpdatedjavascript.js
@@ -57,4 +57,16 @@ function readQuote(){
   })
   
 }
-
+//for the map
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([37.41, 8.82]),
+    zoom: 4
+  })
+});
