e69f9441c4c68df12cd116c6d203c2635f6d5dc4
Johnny Ryue <johnryue1993@gmail.com>
2020-11-07 21:35:43 -0800

    created RestaurantCard.js

---
+++ scripts/RestaurantCard.js
@@ -0,0 +1,18 @@
+function getRestaurantInfoToCards() {
+    db.collection("Restaurants")
+        .get()
+        .then(function (snap) {
+            snap.forEach(function (doc) {
+                var m = doc.data().message;
+                console.log(m);
+                var d1 = $("#restaurant-attributes").append("<div class='card' style='width: 18rem'");
+                var i = d1.append("<img src='...' class='card-img-top' alt='...'>")
+                var d2 = d1.append("<div class='card-body'>")
+                d2.append("<h5 class='card-title'>"+m+"</h5>");
+                d2.append("<p class='card-text'>Based in Vancouver, BC, Canada, Dae-ji restaurant</p>");
+                d2.append("<a href='#' class='btn btn-primary'> Link to something</a>");
+            })
+        })
+}
+
+getRestaurantInfoToCards();
