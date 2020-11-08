5b8fbe00a0a59c52d01da0a6d2f46d6beabac4ae
Kat Sukkau <Katsukkau@gmail.com>
2020-11-07 20:56:02 -0800

    adding js to read/write from database

--- scripts/KatUpdatedjavascript.js
+++ scripts/KatUpdatedjavascript.js
@@ -8,24 +8,50 @@ var textTypedAction = function(event) {
 }
 // for text input
 //window.addEventListener("keydown", textTypedAction, false); // this also works
-document.getElementById("myInput").addEventListener("keydown", textTypedAction, false);
+//document.getElementById("myInput").addEventListener("keydown", textTypedAction, false);
 
 // for clicking on the hamburger icon
-document.getElementById("hamburger-icon").addEventListener("click",
-function(event) { console.log("hambuger icon was clicked on with event: " + event); }, false);
+//document.getElementById("hamburger-icon").addEventListener("click",
+//function(event) { console.log("hambuger icon was clicked on with event: " + event); }, false);
 
 // for clicking on the home icon
-document.getElementById("home-icon").addEventListener("click",
-function(event) { console.log("home icon was clicked on with event: " + event); }, false);
+//document.getElementById("home-icon").addEventListener("click",
+//function(event) { console.log("home icon was clicked on with event: " + event); }, false);
 
 // for clicking on the search icon
-document.getElementById("search-icon").addEventListener("click",
-function(event) { console.log("search icon was clicked on with event: " + event); }, false);
+//document.getElementById("search-icon").addEventListener("click",
+//function(event) { console.log("search icon was clicked on with event: " + event); }, false);
 
 // for clicking on the heart icon
-document.getElementById("heart-icon").addEventListener("click",
-function(event) { console.log("heart icon was clicked on with event: " + event); }, false);
+//document.getElementById("heart-icon").addEventListener("click",
+//function(event) { console.log("heart icon was clicked on with event: " + event); }, false);
 
 // for clicking on the history icon
-document.getElementById("history-icon").addEventListener("click",
-function(event) { console.log("history icon was clicked on with event: " + event); }, false);
+//document.getElementById("history-icon").addEventListener("click",
+//function(event) { console.log("history icon was clicked on with event: " + event); }, false);
+
+//Add a new document to collection retaurant
+db.collection("Restaurants").doc("restaurant02").set({
+  name: "Daji" ,
+  address: "123 main Vancouver" ,
+  business_hours: "9-5 mon- fri",
+  masks_required: "yes" ,
+  count: "25" 
+})
+
+.then(function(){
+  console.log("document successfully written");
+})
+
+.catch(function(error){
+  console.error("error writing document: ", error);
+})
+
+function readQuote(){
+  db.collection("Restaurants").doc("Restaurant01")
+  .onSnapshot(function(snap){
+    console.log(snap.data());  //prints the document fields of "01"
+    console.log(snap.data().name);
+    document.getElementById("abc").innerText = snap.data().name;
+  })
+}
---
+++ scripts/firebase_api.js
@@ -0,0 +1,28 @@
+
+
+
+//TODO: Add SDKs for Firebase products that you want to use
+   //  https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.0.1/firebase-analytics.js"></script>
+
+
+ 
+  var firebaseConfig = {
+    apiKey: "AIzaSyA3Qlv-T2A_ox-vv95MT9xw13tuU3NVsAw",
+    authDomain: "comp1800-team26.firebaseapp.com",
+    databaseURL: "https://comp1800-team26.firebaseio.com",
+    projectId: "comp1800-team26",
+    storageBucket: "comp1800-team26.appspot.com",
+    messagingSenderId: "921630965539",
+    appId: "1:921630965539:web:d804705d0ad4095b489696",
+    measurementId: "G-1DR37029JH"
+  };
+
+ /*Initialize Firebase*/
+
+firebase.initializeApp(firebaseConfig);
+
+// Create the Firestore database object
+// Henceforce, any reference to the database can be made with "db"
+
+const db = firebase.firestore();
