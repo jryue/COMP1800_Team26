66ccdc596b5c112ab52a0aacc4f358d77be3a222
Sam Yoo <syoo0295@gmail.com>
2020-11-07 21:56:35 -0800

    login page

---
+++ scripts/login.js
@@ -0,0 +1,37 @@
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+var uiConfig = {
+    callbacks: {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+            var user = authResult.user;
+            if (authResult.additionalUserInfo.isNewUser) {
+                db.collection("user").doc(user.uid).set({
+                        name: user.displayName,
+                        email: user.email
+                    }).then(function () {
+                        console.log("New user added to firestore");
+                        window.location.assign("home.html");
+                    })
+                    .catch(function (error) {
+                        console.log("Error adding new user: " + error);
+                    });
+            } else {
+                return true;
+            }
+            return false;
+        },
+        uiShown: function () {
+            document.getElementById('loader').style.display = 'none';
+        }
+    },
+
+    signInFlow: 'popup',
+    signInSuccessUrl: 'home.html',
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    tosUrl: 'home.html',
+    privacyPolicyUrl: 'home.html',
+    accountChooserEnabled: false
+};
+ui.start('#firebaseui-auth-container', uiConfig);
