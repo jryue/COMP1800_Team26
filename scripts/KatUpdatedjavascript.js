196df20a528a1967cac187ce2d95932480a3cc40
Kat Sukkau <Katsukkau@gmail.com>
2020-11-07 22:00:24 -0800

    corrected map starting point

--- scripts/KatUpdatedjavascript.js
+++ scripts/KatUpdatedjavascript.js
@@ -66,7 +66,7 @@ var map = new ol.Map({
     })
   ],
   view: new ol.View({
-    center: ol.proj.fromLonLat([37.41, 8.82]),
+    center: ol.proj.fromLonLat([-123.11, 49.25]),
     zoom: 4
   })
 });
