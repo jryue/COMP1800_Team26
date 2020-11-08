7d8ecbba75402f975754c5ced8471fe79267bbe7
Johnny Ryue <johnryue1993@gmail.com>
2020-11-07 21:50:25 -0800

    changes to js and html files

--- scripts/RestaurantCard.js
+++ scripts/RestaurantCard.js
@@ -6,8 +6,11 @@ function getRestaurantInfoToCards() {
                 var m = doc.data().message;
                 console.log(m);
                 var d1 = $("#Restaurant02").append("<div class='daeji' style='width: 18rem'");
+                console.log(d1);
                 var i = d1.append("<img src='...' class='card-img-top' alt='...'>")
+                console.log(i);
                 var d2 = d1.append("<div class='card-body'>")
+                console.log(d2);
                 d2.append("<h5 class='card-title'>"+m+"</h5>");
                 d2.append("<p class='card-text'>Based in Vancouver, BC, Canada, Dae-ji restaurant</p>");
                 d2.append("<a href='#' class='btn btn-primary'> Link to something</a>");
