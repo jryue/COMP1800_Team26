32d2baadec0afa9a13a426ce065f40008f9edfad
Johnny Ryue <johnryue1993@gmail.com>
2020-11-13 12:16:48 -0800

    edited RestaurantCard.js

--- scripts/RestaurantCard.js
+++ scripts/RestaurantCard.js
@@ -8,7 +8,8 @@ function getRestaurantCard() {
                 var d1 = $('#restaurant').append('<div class="rest-container">');
                 var i = d1.append('<div id="pic">');
                 var image1 = i.append('<img src="/images/DaeJi_4883_Burnaby_BC.png" alt="Dae-Ji restaurant">');
-                var title = d1.append('<h1 class="display-4">' + m + '</h1>');
+                var title = d1.append('<h1 class="display-4"></h1>');
+                document.getElementByClass("display-4").innerText = snap.data(m).title;
                 var info = d1.append('<div id="info">');
                 var maskreq = info.append('<div class="masks">' + m + '</div>');
                 var hoursop = info.append('<div class="hours">' + m + '</div>');
