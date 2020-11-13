17fd46352e984dca0059045a957f805a70fb3529
Johnny Ryue <johnryue1993@gmail.com>
2020-11-13 11:58:36 -0800

    javascript to restaurant info

--- scripts/RestaurantCard.js
+++ scripts/RestaurantCard.js
@@ -1,27 +1,23 @@
 function getRestaurantInfo() {
     db.collection("Restaurants")
-        .doc("Restaurant02")
         .get()
-        .then(function(doc) {
-            console.log("The data for this restaurant is:", doc.data());
-            //parseDetails(doc);
-            })
+        .then(function(snap) {
+            snap.forEach(function(doc) {
+                var m = doc.data().message;
+                //console.log(m);
+                var d1 = $('#restaurant').append('<div class="rest-container">');
+                var i = d1.append('<div id="pic">');
+                var image1 = i.append('<img src="/images/DaeJi_4883_Burnaby_BC.png" alt="Dae-Ji restaurant">');
+                var title = d1.append('<h1 class="display-4">' + m + '</h1>');
+                var info = d1.append('<div id="info">');
+                var maskreq = info.append('<div class="masks">' + m + '</div>');
+                var hoursop = info.append('<div class="hours">' + m + '</div>');
+                var contactinfo = info.append('<div class="contact">' + m + '</div>');
+                var pplcount = info.append('<div class="person-counter">' + m + '</div>');
+            
 
-        var count = 0;
-
-        doc.forEach(function(x) {
-            $("#restaurant").append('<div id="info"> <div class="masks">' + count
-            + '"</div><div class="hours">"' + count + '</div><div class="contact">'
-            + count + '</div><div class="person-counter">' + count + '</div>');
-        }
-        /* function parseDetails(doc) {
-            let maskreq = document.createElement('masks');
-            let hour = document.createElement('hour');
-            let contact = document.createElement('contact');
-            let person = document.createElement('counter');
-
-            mask.textContent(''); */
-        
-
-
-//getRestaurantInfo();
+        //.where('where maches text').get()
+        })
+    })
+}
+getRestaurantInfo();
