0d4dbc4118ccfedba1d5823dc60685a317403abe
Sam Yoo <syoo0295@gmail.com>
2020-11-20 10:50:15 -0800

    combine everything into one review.html

--- scripts/rating.js
+++ scripts/rating.js
@@ -1,8 +1,11 @@
-const btn = document.querySelector("button");
+const btn = document.querySelector("#button");
 const post = document.querySelector(".post");
 const widget = document.querySelector(".star-rating");
+const submit = document.querySelector(".modal-footer")
+
 btn.onclick = () => {
     widget.style.display = "none";
     post.style.display = "block";
+    submit.style.display = "none";
     return false;
 }
