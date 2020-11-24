4280f15aaa36e89b8790769ca20f9d2a32259d41
Sam Yoo <syoo0295@gmail.com>
2020-11-24 01:33:18 -0800

    review

--- scripts/rating.js
+++ scripts/rating.js
@@ -1,11 +1,69 @@
 const btn = document.querySelector("#button");
 const post = document.querySelector(".post");
+const text = document.querySelector(".review-text")
 const widget = document.querySelector(".star-rating");
-const submit = document.querySelector(".modal-footer")
+const footer = document.querySelector(".modal-footer");
+const container = document.querySelector('.rating');
+const items = container.querySelectorAll('.rating-item');
 
-btn.onclick = () => {
+function storeReview() {
+    var review = document.querySelector(".userReview").value;
+
+    //set review date as today's date
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+
+    //hide buttons if submit
+    text.style.display = "none";
     widget.style.display = "none";
     post.style.display = "block";
-    submit.style.display = "none";
-    return false;
+    footer.style.display = "none";
+
+    db.collection("review").doc().set({
+            userReview: review,
+            reviewDate: today
+        })
+        .then(function () {
+            console.log("Document successfully written!");
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
+        });
 }
+container.onclick = e => {
+    const elClass = e.target.classList;
+    if (!elClass.contains('active')) {
+        items.forEach(
+            item => item.classList.remove('active')
+        );
+        console.log(e.target.getAttribute("data-rate"));
+        elClass.add('active');
+    }
+}
+// // Initial Ratings
+// const ratings = {
+//     userRate: 4.5
+// }
+
+// // Total Stars
+// const starsTotal = 5;
+
+// // Run getRatings when DOM loads
+// document.addEventListener('DOMContentLoaded', getRatings);
+
+// // Get ratings
+// function getRatings() {
+//     for (let rating in ratings) {
+//         // Get percentage
+//         const starPercentage = (ratings[rating] / starsTotal) * 100;
+
+//         // Round to nearest 10
+//         const starPercentageRounded = `${Math.round((starPercentage / 10) * 10)}%`;
+
+//         // Set width of stars-inner to percentage
+//         document.querySelector('.stars-inner').style.width = starPercentageRounded;
+//     }
+// }
---
+++ scripts/review.js
@@ -0,0 +1,30 @@
+function getRestaurantProfile() {
+    db.collection("Restaurants")
+        .where("Name", "==", "Dae-Ji")
+        .get()
+        .then(function (snap) {
+            snap.forEach(function (doc) {
+                console.log(doc.data());
+                var name = doc.data().Name;
+                var address = doc.data().Address;
+
+                var restaurantName = $('.restuarnt-name').append(name);
+                var restaurantAddress = $('.restaurant-address').append(address);
+            })
+        })
+}
+
+getRestaurantProfile();
+
+function getRestaurantImage() {
+    db.collection("image")
+        .doc("pic")
+        .get()
+        .then(function (snap) {
+            snap.forEach(function (doc) {
+                var img = doc.data().img_url;
+            })
+        })
+}
+
+getRestaurantImage;
